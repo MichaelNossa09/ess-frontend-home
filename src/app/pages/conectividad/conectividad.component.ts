6b15104d373d809c52cdc82bc2083f1d3f2d5309
Michael Nossa <auxdesarrollo@agrobanacaribe.com>
2024-01-08 16:30:37 -0500

    Subiendo Notificaciones & AngularMaterial

---
+++ src/app/pages/conectividad/conectividad.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conectividad',
+  standalone: true,
+  imports: [],
+  templateUrl: './conectividad.component.html',
+  styleUrl: './conectividad.component.css'
+})
+export class ConectividadComponent {
+
+}
